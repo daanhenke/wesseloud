cc82d29ff8211d9f6e541a8f3b0666620f27c060
daanhenke <daanhenke@users.noreply.github.com>
2022-06-04 14:43:36 +0000

    deploy: 30443e573e9fe9837dfd57eccf8df15b2f8d6d6a

--- _nuxt/static/1654353259/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1654353259",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"39a7773b"}}}}(null,"\u002F"));
